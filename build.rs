7b65f7bf229f02ea066a234ae266506232f45bbf
Matsuoka Takashi <mt@xfutures.jp>
2026-01-02 16:52:14 +0900

    feat: require admin permission on startup

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 fn main() {
     #[cfg(target_os = "windows")]
     {
-        let _ = embed_resource::compile("windns.manifest", embed_resource::NONE);
         let _ = embed_resource::compile("windns.rc", embed_resource::NONE);
     }
 }
